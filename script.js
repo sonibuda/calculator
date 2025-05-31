362dafdaf5bc12838b91c2b1b1e5189b9c7e0e9e
Soni Buda <181140941+sonibuda@users.noreply.github.com>
2025-05-31 08:51:37 -0600

    Add html for calculator buttons.

--- script.js
+++ script.js
@@ -14,4 +14,24 @@ function divide(n1, n2){
     return n1 / n2;
 }
 
-//probably update multplication & division with array methods.
+const n1 = 0;
+const n2 = 0;
+const operator = '+';
+
+function operate(n1, n2, operator){
+    switch(operator){
+        case '+':
+            return add(n1, n2);
+        case '-':
+            return subtract(n1, n2);
+        case '*':
+            return multiply(n1, n2);
+        case '/':
+            return divide(n1, n2);
+        default:
+            alert('Please choose a valid operator, such as +, -, *, or /.');
+            return null;
+    }
+ }
+
+
